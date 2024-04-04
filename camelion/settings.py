4fc6063901ec3c9bae5bd845b829712644b42a49
NephygTdkingz <tdkingzict@gmail.com>
2024-04-04 16:32:56 +0100

    changed media root to mounted disk

--- camelion/settings.py
+++ camelion/settings.py
@@ -148,7 +148,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL =  '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = "/var/lib/data"
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
